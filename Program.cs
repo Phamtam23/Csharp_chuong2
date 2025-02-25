628b9f760d48fdfd7150fbe37eefefc7406a0fe1
Phamtam23 <phamthientam20041@gmail.com>
2025-02-25 15:57:07 +0700

    thêm hàm tính tổng có dùng optional

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ namespace chuong2
 {
     class Program
     {
+        // Hàm tính tổng với tham số b là optional (mặc định là 10)
+        static int TinhTong_336(int a_336, int b_336 = 10)
+        {
+            return a_336 + b_336;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("chuong2");
